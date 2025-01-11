a1bbb2e698250007febe41cc1b037910c5021cd9
LethalisUmbra <anibal.cabrera2000@gmail.com>
2025-01-10 22:18:40 -0300

    FIX: Refresh request loop

--- front/src/utils/api.js
+++ front/src/utils/api.js
@@ -27,24 +27,64 @@ export const getUserProfile = async () => {
   return response.data
 }
 
-// Interceptor para manejar la renovación del token
+let isRefreshing = false
+let failedQueue = []
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach(prom => {
+    if (error) {
+      prom.reject(error)
+    } else {
+      prom.resolve(token)
+    }
+  })
+
+  failedQueue = []
+}
+
 api.interceptors.response.use(
   (response) => response,
   async (error) => {
     const originalRequest = error.config
+
     if (error.response.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({ resolve, reject })
+        }).then(token => {
+          return api(originalRequest)
+        }).catch(err => {
+          return Promise.reject(err)
+        })
+      }
+
       originalRequest._retry = true
+      isRefreshing = true
+
       try {
         await refreshToken()
+        // No necesitamos manejar el token aquí, ya que se establece en las cookies
+        processQueue(null)
         return api(originalRequest)
       } catch (refreshError) {
-        // Si no se puede refrescar el token, redirigir al login
-        window.location.href = '/login'
+        processQueue(refreshError, null)
+        // Redirigir al login y limpiar el estado de autenticación
+        clearAuthState()
+        Router.push('/login')
         return Promise.reject(refreshError)
+      } finally {
+        isRefreshing = false
       }
     }
+
     return Promise.reject(error)
   }
 )
+
+export const clearAuthState = () => {
+  Cookies.remove('access-token')
+  Cookies.remove('refresh-token')
+  // Aquí puedes agregar cualquier otra limpieza necesaria
+}
 
 export default api
